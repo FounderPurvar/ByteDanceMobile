a1526a77b483e4cf173da0442c6471633d72d653
liuchao2018 <610512810@qq.com>
2018-03-19 12:02:04 +0800

    毛睿提交版0318

---
+++ mobile/www/js/config/config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2015/12/10.
+ */
+document.write('<script src="js/config/config-url.js"><\/script>');
+document.write('<script src="forms/HR/HR_BIRTH_WELFARE_APPLICATION/HR_BIRTH_WELFARE_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_SELF_PAYMENT_APPLICATION/FA_SELF_PAYMENT_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_SMB_CUSTOMER_CANCEL_REFUND_APPLICATION/FA_SMB_CUSTOMER_CANCEL_REFUND_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_SMB_CUSTOMER_REFUND_APPLICATION/FA_SMB_CUSTOMER_REFUND_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_PURCHASE_AT_EASE_BAND_REFUND_APPLICATION/FA_PURCHASE_AT_EASE_BAND_REFUND_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_PURCHASE_USER_INDEMNITY_APPLICATION/FA_PURCHASE_USER_INDEMNITY_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_NON_COMMERCIAL_INVOICE_APPLICATION/FA_NON_COMMERCIAL_INVOICE_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_SEAL_CERT_USE_APPLICATION/FA_SEAL_CERT_USE_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_SEAL_CERT_LENT_APPLICATION/FA_SEAL_CERT_LENT_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_INVESTMENT_PROJECT_APPLICATION/FA_INVESTMENT_PROJECT_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/PJ/PJ_NEW_PROJECT_APPLICATION/PJ_NEW_PROJECT_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/Contract/C_CONTRACT_APPLICATION/C_CONTRACT_APPLICATION_config.js"><\/script>');
+document.write('<script src="forms/financeManagement/FA_COST_APPLICATION/FA_COST_APPLICATION_config.js"><\/script>');
+
+var FormConfig = {};
+var selectGroup = {};
+selectGroup.flowDef = [{
+    label: "生育福利申请单",
+    options: [{
+        value: "",
+        text: "生育福利申请单"
+    }]
+}];
+
